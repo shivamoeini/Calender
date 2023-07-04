c0c47686e6a1c8c0b67d9c70e9dc6ed8720d0767
shiva moeini <87569940+shivamoeini@users.noreply.github.com>
2023-07-04 14:12:35 +0330

    calender

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Scheduler from './components/Scheduler';
+import Toolbar from './components/Toolbar';
+import MessageArea from './components/MessageArea';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const data = [
+    { start_date:'2020-06-10 6:00', end_date:'2020-06-10 8:00', text:'Event 1', id: 1},
+    { start_date:'2020-06-13 10:00', end_date:'2020-06-13 18:00', text:'Event 2', id: 2 }
+];
 
+class App extends Component {
+    state = {
+        currentTimeFormatState: true,
+        messages: []
+    };
+
+    addMessage(message) {
+        const maxLogLength = 5;
+        const newMessage = { message };
+        const messages = [
+            newMessage,
+            ...this.state.messages
+        ];
+
+        if (messages.length > maxLogLength) {
+            messages.length = maxLogLength;
+        }
+        this.setState({ messages });
+    }
+
+    logDataUpdate = (action, ev, id) => {
+        const text = ev && ev.text ? ` (${ev.text})` : '';
+        const message = `event ${action}: ${id} ${text}`;
+        this.addMessage(message);
+    }
+
+    handleTimeFormatStateChange = (state) => {
+        this.setState({
+            currentTimeFormatState: state
+        });
+    }
+
+    render() {
+        const { currentTimeFormatState, messages } = this.state;
+        return (
+            <div>
+                <div className="tool-bar">
+                    <Toolbar
+                        timeFormatState={currentTimeFormatState}
+                        onTimeFormatStateChange={this.handleTimeFormatStateChange}
+                    />
+                </div>
+                <div className='scheduler-container'>
+                    <Scheduler
+                        events={data}
+                        timeFormatState={currentTimeFormatState}
+                        onDataUpdated={this.logDataUpdate}
+                    />
+                </div>
+                <MessageArea
+                    messages={messages}
+                />
+            </div>
+        );
+    }
+}
 export default App;
